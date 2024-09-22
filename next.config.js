59c64b90cbdea2d07dc71ebadb3ce75814aca6e9
CHENJuntong <chen_juntong@foxmail.com>
2024-09-23 00:39:20 +0800

    update

--- next.config.js
+++ next.config.js
@@ -1,5 +1,22 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
   images: {
     domains: ['www.notion.so', 'lh5.googleusercontent.com', 's3-us-west-2.amazonaws.com'],
   },
-}
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, s-maxage=10, stale-while-revalidate=59',
+          },
+        ],
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
